47b34a733126ed3168794f449a956827e14670c6
esteramine <cchouaa@connect.ust.hk>
2022-10-19 17:54:32 +0800

    update (caters to different team's requirement)

--- rvtm_transform.py
+++ rvtm_transform.py
@@ -3,32 +3,35 @@ from os import listdir
 from os.path import isfile, join
 from openpyxl import load_workbook
 
+print('Start...')
 # read all excel files in the current folder
 files = [f for f in listdir('./') if isfile(join('./', f)) and f.split('.')[-1] == 'xlsx'] # TODO: more generalized path
 os.mkdir('modified')
 for file in files:
     workbook = load_workbook(filename=file)
-    sheet = workbook['需求清單']
+    if '需求清單' in workbook.sheetnames[0]:
+        print(file)
+        sheet = workbook[workbook.sheetnames[0]]
 
-    # modify the R1 cell with 'Interdependency'
-    if sheet['R1'].value == 'Independency':
-        sheet['R1'] = 'Interdependency'
-    
-    # delete column N ~ Q
-    if sheet['N3'].value == '最終須\n投入資源' and sheet['Q3'].value == '建議不納入範圍說明':
-        # unmerge cells L1~Q1, L2~Q2, R1~U1, R2~U2
-        sheet.unmerge_cells('L1:Q1')
-        sheet.unmerge_cells('L2:Q2')
-        sheet.unmerge_cells('R1:U1')
-        sheet.unmerge_cells('R2:U2')
-
-        sheet.delete_cols(14, 4)
-
-        # unmerge cells L1~M1, L2~M2, N1~Q1, N2~Q2
-        sheet.merge_cells('L1:M1')
-        sheet.merge_cells('L2:M2')
-        sheet.merge_cells('N1:Q1')
-        sheet.merge_cells('N2:Q2')
+        # modify the R1 cell with 'Interdependency'
+        if sheet['R1'].value == 'Independency':
+            sheet['R1'] = 'Interdependency'
         
-    workbook.save(filename='modified/'+file)
+        # delete column N ~ Q
+        if sheet['N3'].value == '最終須\n投入資源' and sheet['Q3'].value == '建議不納入範圍說明':
+            # unmerge cells L1~Q1, L2~Q2, R1~U1, R2~U2
+            sheet.unmerge_cells('L1:Q1')
+            sheet.unmerge_cells('L2:Q2')
+            sheet.unmerge_cells('R1:U1')
+            sheet.unmerge_cells('R2:U2')
+
+            sheet.delete_cols(14, 4)
+
+            # unmerge cells L1~M1, L2~M2, N1~Q1, N2~Q2
+            sheet.merge_cells('L1:M1')
+            sheet.merge_cells('L2:M2')
+            sheet.merge_cells('N1:Q1')
+            sheet.merge_cells('N2:Q2')
+            
+        workbook.save(filename='modified/'+file)
 
