50e4f52a71302149b307f09ebef3f7e080717286
esteramine <cchouaa@connect.ust.hk>
2022-10-24 15:03:20 +0800

    deleted useless code and changed getting api to directly reading the result json file

--- jira/actual_completion.groovy
+++ jira/actual_completion.groovy
@@ -2,20 +2,22 @@ import groovy.lang.*
 import groovy.json.JsonSlurper
 
 def project = "NEW"
-def getfield = get('/rest/api/2/field').asString().body
+// def getfield = get('/rest/api/2/field').asString().body
+def fieldFile = new File("jira_fields.json")
 def slurper = new JsonSlurper()
-def fields = slurper.parseText(getfield);
+def fields = slurper.parseText(fieldFile.text)
 def StorypointsField = fields.find { it.name == "Story Points" }?.id
 def PlanPercentage = fields.find { it.name == "Plan Percentage" }?.id
 def percentageField = fields.find { it.name == "Percentage" }?.id
 def PlanEndDate = fields.find { it.name == "Plan end date" }?.id
 def issuetypeField = fields.find { it.name == "issuetype" }?.id
 def d = new Date()
-//println d.format("YYYY-MM-dd")
-println  StorypointsField
+
 //Get all issue
-def allissue = get ("rest/api/3/search?jql=project%20%3D%20"+project+"%20").asString().body
-def issue = slurper.parseText(allissue);
+// def allissue = get ("rest/api/3/search?jql=project%20%3D%20"+project+"%20").asString().body
+// def all = slurper.parseText(allissue)
+def file = new File("jira_issues.json")
+def issue = slurper.parseText(file.text)
 def issuetype=issue.issues.fields.issuetype.name
 def storypointvalue=issue.issues.fields[StorypointsField]
 def percentage = issue.issues.fields[percentageField]
@@ -31,42 +33,7 @@ int level(String name) { // TODO: soft code (get level from json result)
     if (name == 'Sub-task') return 0
 }
 
-// println "Sorted Result:"
-// def sortedObj = issue.issues.sort{ a,b -> level(b.fields.issuetype.name) <=> level(a.fields.issuetype.name)}
-// for (int i = 0; i < sortedObj.size(); i++) {
-//     println sortedObj[i].fields.issuetype.name
-// }
-
 def issues = issue.issues
-// save to different list
-// def phases = []
-// def sdlcs = []
-// def epics = []
-// def stories = []
-// def subtasks = []
-
-// for (int i = 0; i < issues.size(); i++) {
-//     if (level(issues[i].fields.issuetype.name) == 4) {
-//         phases.add(issues[i])
-//     }
-//     else if (level(issues[i].fields.issuetype.name) == 3) {
-//         sdlcs.add(issues[i])
-//     }
-//     else if (level(issues[i].fields.issuetype.name) == 2) {
-//         epics.add(issues[i])
-//     }
-//     else if (level(issues[i].fields.issuetype.name) == 1) {
-//         stories.add(issues[i])
-//     }
-//     else if (level(issues[i].fields.issuetype.name) == 0) {
-//         subtasks.add(issues[i])
-//     }
-// }
-// for (int i = 0; i < subtasks.size(); i++) {
-//     println subtasks[i].fields.issuetype.name
-//     println subtasks[i].fields.issuetype.id
-//     println subtasks[i].id
-// }
 
 // save to different maps
 def phases = [:]
@@ -74,38 +41,6 @@ def sdlcs = [:]
 def epics = [:]
 def stories = [:]
 def subtasks = [:]
-
-
-// 1. save the complete issue to map
-// for (int i = 0; i < issues.size(); i++) {
-//     if (level(issues[i].fields.issuetype.name) == 4) {
-//         phases[issues[i].id] = issues[i]
-//     }
-//     else if (level(issues[i].fields.issuetype.name) == 3) {
-//         sdlcs[issues[i].id] = issues[i]
-//     }
-//     else if (level(issues[i].fields.issuetype.name) == 2) {
-//         epics[issues[i].id] = issues[i]
-//     }
-//     else if (level(issues[i].fields.issuetype.name) == 1) {
-//         stories[issues[i].id] = issues[i]
-//     }
-//     else if (level(issues[i].fields.issuetype.name) == 0) {
-//         subtasks[issues[i].id] = issues[i]
-//     }
-// }
-
-// stories.each { key, val ->
-//     println key
-//     println val.fields.issuetype.name
-//     println "Subtasks"
-//     def total = 0
-//     for (id in val.fields.subtasks.id) {
-//         println id
-//         // total += storypoints
-//     }
-//     println "\n"
-// }
 
 // 2. save only list position to map
 for (int i = 0; i < issues.size(); i++) {
